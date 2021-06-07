b24e972a9a34017c259f3186487d008a64a3476b
tanishagoyal2 <tanisha.goyal.928@gmail.com>
2021-06-07 17:43:56 +0530

    problem solved

--- src/dynamic/SubsetSumProblem.java
+++ src/dynamic/SubsetSumProblem.java
@@ -1,4 +1,7 @@
 package dynamic;
+
+import java.util.ArrayDeque;
+
 public class SubsetSumProblem {
 
 	// not a efficient approach . 
@@ -74,11 +77,70 @@ public class SubsetSumProblem {
 //print the subsets possible with given sum
 public static void printSubset(int arr[],int sum,int n){
 
+	boolean dp[][]=new boolean [n+1][sum+1];
+	for(int i=0;i<=n;i++){
+		for(int j=0;j<=sum;j++){
+			if(i==0 && j==0){
+				dp[i][j]=true;
+			}
+			else if(i==0){
+				dp[i][j]=false;
+			}
+			else if(j==0){
+				dp[i][j]=true;
+			}
+			else{
+				if(dp[i-1][j]==true) {
+					dp[i][j]=true;
+				}
+				else {
+					int val=arr[i-1];
+					if(val<=j) {
+						dp[i][j]=dp[i-1][j-val];
+					}
+				}
+			}
+			System.out.println(dp[i][j]);
+		}
+	}
+	System.out.println(dp[n][sum]);
+
+	ArrayDeque<Pair> queue=new ArrayDeque<Pair>();
+
+	queue.add(new Pair(n,sum,""));
+
+	while(queue.size()>0){
+		Pair rem=queue.removeFirst();
+		if(rem.i==0 || rem.j==0){
+			System.out.println(rem.psf);
+		}
+		else{
+			if(rem.j>=arr[rem.i-1]){
+				boolean inc=dp[rem.i-1][rem.j-arr[rem.i-1]];
+				if(inc){
+					queue.add(new Pair(rem.i-1,rem.j-arr[rem.i-1],(rem.i-1)+" "+rem.psf));
+				}
+			}
+			boolean exc=dp[rem.i-1][rem.j];
+			if(exc)
+			queue.add(new Pair(rem.i-1,rem.j,rem.psf));
+		}
+	}
 }
 
 	public static void main(String args[]) {
-		int set[] = {2,5,3};
-        int sum = 5;
-        subsetNumbers(set,sum,3);
+		int set[] = {4,2,7,1,3};
+        int sum = 10;
+        printSubset(set,sum,5);
+	}
+}
+class Pair{
+	int i;
+	int j;
+	String psf;
+	Pair(int a,int b,String str){
+		this.i=a;
+		this.j=b;
+		this.psf=str;
 	}
 }
